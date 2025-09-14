d6fa542315783a161ca57075b50529f42799fa86
Ashish  Sah <ashishsah@CKMAC168.local>
2025-09-14 11:24:55 +0530

    assign crdential in server file

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,13 @@ const cors = require('cors');                           // Built-in module to in
 const analyzeRoute = require('./routes/analyze');  // Importing the analyze route module
 const enhanceRoute = require('./routes/enhanceResume');
 const statsRoutes = require('./routes/stats');      // Importing the stats route module
-
+require('dotenv').config({ path: require('path').resolve(__dirname, './.env') });
 // Initialize the Express application
 const app = express();
 const PORT = process.env.PORT;
 
 // Middleware setup
-app.use(cors());
+app.use(cors({ origin: true, credentials : true })); // Allow all origins for simplicity; adjust in production
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
